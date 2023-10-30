9dfb16352c1698077eac9fc6e152d90a93029234
CK Ng <ngchoonkhon@gmail.com>
2023-10-30 20:49:52 +0800

    Update code

--- ml-server/rpc_server.py
+++ ml-server/rpc_server.py
@@ -2,13 +2,6 @@
 import pika
 import random
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='rpc_queue')
-
 def classificationModel(input):
     """
     Fake binary classification model
@@ -31,6 +24,13 @@ def on_request(ch, method, props, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 if __name__ == '__main__':
+    connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue='rpc_queue')
+
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
 
